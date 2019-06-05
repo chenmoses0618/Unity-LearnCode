6d593726ddf94af61d93de71f328be9ffcf738b4
chenmoses0618 <chensmiley@gmail.com>
2019-06-05 15:56:59 +0800

    戰士屬性採用get與set做法

--- UnityLearnCode/Assets/練習2/gameManager.cs
+++ UnityLearnCode/Assets/練習2/gameManager.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
-
+//using Warrior;
 public class gameManager : MonoBehaviour
 {
     public Solider SoldierA = new Solider();
+    public Warrior WarriorA = new Warrior();
     //起始點
     void Start()
     { 
@@ -11,6 +12,10 @@ public class gameManager : MonoBehaviour
         Debug.Log("士兵經驗值-取得:" + SoldierA.GetExp());
         SoldierA.SetExp(100);
         Debug.Log("士兵經驗值-設定:" + SoldierA.exp);
+        WarriorA.lv = 50;
+        Debug.Log("戰士經驗值-取得:" + WarriorA.Exp);
+        WarriorA.lv = 777;
+        Debug.Log("戰士經驗值-取得:" + WarriorA.Exp);
     }
  
 }
--- UnityLearnCode/Assets/練習2/warrior.cs
+++ UnityLearnCode/Assets/練習2/warrior.cs
@@ -5,5 +5,18 @@ public class Warrior : MonoBehaviour
 {
     public int exp;
     public int lv;
-    public int Exp { get; set; }
+    //設定成屬性方法,透過get與set預設方法設定屬性
+    public int Exp
+    {
+        get
+        {
+            exp = lv * 10;
+            return exp;
+
+        }
+        set
+        {
+            exp = value;
+        }
+    }
 }
