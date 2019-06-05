c0c5ef90b46f49b2569b5dda5560e4145d9f1bfd
chenmoses0618 <chensmiley@gmail.com>
2019-06-05 11:43:37 +0800

    宣告狗類別與方法
    使用UNITY的API重寫類別與方法

---
+++ UnityLearnCode/Assets/script/praticle_6_5.cs
@@ -0,0 +1,31 @@
+﻿#region  引用unity API
+using UnityEngine;
+
+#endregion
+//5-29 c#練習	
+public class praticle_6_5
+{
+   
+    
+    //宣告狗類別
+    public class Dog
+    {
+        #region  宣告屬性欄位
+        public string name;
+        public float weight;
+        public string kind;
+        public string sex;
+        public string color;
+        public int age;
+        #endregion
+        #region  宣告方法-動物叫聲(無回傳值)
+        public void voice()
+        {
+            Debug.Log(name + ":汪汪");
+          
+        }
+        #endregion
+
+        
+    }
+}
