f773fdf9ec3817f6b4944f5eb39c82e62a4eab91
chenmoses0618 <chensmiley@gmail.com>
2019-06-12 16:31:29 +0800

    增加備註說明

--- CardGame/卡牌練習/Assets/Card.cs
+++ CardGame/卡牌練習/Assets/Card.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public class Card
 {
     #region 封裝所隱藏之私有變數
-    private int _cardNum;
-    private float _hposition;
-    private Sprite _cardPic;
+    private int _cardNum;//私有卡牌數字
+    private float _hposition;//私有水平位置
+    private Sprite _cardPic;//私有卡面圖片
     #endregion
     #region 封裝卡牌之讀寫屬性
     public int CardNum { get => _cardNum; }
@@ -17,9 +17,8 @@ public class Card
     public Card(int card_num,float card_hpostion,Sprite card_pic)
     {
         _cardNum = card_num;
-        CardHposition = card_hpostion;
+        _hposition = card_hpostion;
         _cardPic= card_pic;
-
     }
     #endregion
     #region 多載空白建構子-無參數
