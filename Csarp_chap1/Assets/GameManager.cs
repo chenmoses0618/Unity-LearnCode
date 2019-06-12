d4ba60711317f6ac229812681b36c5437b97dee9
chenmoses0618 <chensmiley@gmail.com>
2019-06-12 09:55:14 +0800

    屬性讀寫與封裝

---
+++ Csarp_chap1/Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class GameManager : MonoBehaviour //必須繼承monobehaviour才能被unity物件使用
+{
+    public Ship ship = new Ship();  //產生ship物件
+    void Start()//遊戲起始事件
+    {
+        ship.speed = 10.5f;//寫入ship物件屬性值
+        Debug.Log("船艦的速度:" + ship.speed);//讀取物件屬性值並且列印出來
+        ship.Length = 30.7f;
+        Debug.Log("船艦的長度:" + ship.Length);//讀取物件屬性值並且列印出來
+        //ship.Width = 111.2f; 唯讀屬性 無法寫入值
+        Debug.Log("船艦的寬度:" + ship.Width);//讀取物件屬性值並且列印出來
+        //Debug.Log("船艦的密碼:" + ship.PassWord); 唯寫屬性無法讀出
+
+
+    }
+}
--- Csarp_chap1/Assets/ship.cs
+++ Csarp_chap1/Assets/ship.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
-public class ship : MonoBehaviour
+public class Ship
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //屬性欄位  修飾詞為public就可供給外部任意讀寫
+    public float speed;
+    //prop點兩下按Tab快速產生屬性
+    //成員名稱物能重複:欄位屬性方法
+    public float Length { get; set; }//包含讀寫屬性get&set
+                                     //public float Width { get; }    //僅有讀取屬性get
+                                     //因為預設不能僅有唯寫屬性,所以一般使用上不存在
+                                     // public int PassWord {set; }    //僅有唯寫set屬性
+    //設定私有屬性
+    private float _Width = 6.18f;
+    //回傳封裝後私有屬性
+    public float Width{get { return _Width; }}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
