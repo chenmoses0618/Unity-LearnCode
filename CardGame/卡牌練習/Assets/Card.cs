bc5232aed73381960c570c4bdb2dd5ab71d689c5
chenmoses0618 <chensmiley@gmail.com>
2019-06-12 15:31:32 +0800

    卡牌封裝屬性與建構子

---
+++ CardGame/卡牌練習/Assets/Card.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class Card
+{
+    #region 封裝所隱藏之私有變數
+    private int _cardNum;
+    private float _hposition;
+    private Sprite _cardPic;
+    #endregion
+    #region 封裝卡牌之讀寫屬性
+    public int CardNum { get => _cardNum; }
+    public float CardHposition { get => _hposition; set => _hposition = value; }
+    public Sprite CardPic { get => _cardPic; }
+    #endregion
+    #region 自訂建構子寫入參數(卡牌數字,牌之水平位置,牌面圖片)
+    public Card(int card_num,float card_hpostion,Sprite card_pic)
+    {
+        _cardNum = card_num;
+        CardHposition = card_hpostion;
+        _cardPic= card_pic;
+
+    }
+    #endregion
+    #region 多載空白建構子-無參數
+    public Card()
+    {
+    }
+    #endregion
+
+}
---
+++ CardGame/卡牌練習/Assets/CardGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class CardGame : MonoBehaviour
+{
+    public Card card1 = new Card();
+    void Start()
+    {
+        
+    }
+
+    
+}
