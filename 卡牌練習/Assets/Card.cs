30f320ac3ec389a9889ea73fa6aeb1747e5f9a1e
chenmoses0618 <chensmiley@gmail.com>
2019-06-19 12:54:50 +0800

    移動卡牌練習專案

---
+++ 卡牌練習/Assets/Card.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+[System.Serializable]//類別序列化:顯示屬性面板
+public class Card
+{
+    #region 封裝所隱藏之私有變數
+    private int _cardNum;//私有卡牌數字
+    private float _hposition;//私有水平位置
+    private Sprite _cardPic;//私有卡面圖片
+    #endregion
+    #region 封裝卡牌之讀寫屬性
+    public int CardNum { get => _cardNum; }
+    public float CardHposition { get => _hposition; set => _hposition = value; }
+    public Sprite CardPic { get => _cardPic; }
+    #endregion
+    #region 自訂建構子寫入參數(卡牌數字,牌之水平位置,牌面圖片),並建立卡片物件載入初始玩家與電腦牌面
+    public Card(int card_num,float card_hpostion,Sprite card_pic,string player)
+    {
+        #region 傳入建構子讀取變數寫入私有變數
+        _cardNum = card_num;
+        _hposition = card_hpostion;
+        _cardPic= card_pic;
+        #endregion
+        #region 建立Unity之遊戲物件,物件名稱名為"卡片",將現有之圖片放入遊戲物件病毒取圖片之花紋與位置
+        GameObject Card = new GameObject(player);//Unity Api遊戲物件名稱
+        Card.AddComponent<SpriteRenderer>();//泛型必須透過<類型>添加元件;在Unity顯示兩張卡片物件,用以顯示卡片物件
+        Card.GetComponent<SpriteRenderer>().sprite=CardPic;//卡片取得圖片元件cardpic之屬性
+        Card.GetComponent<Transform>().position = new Vector2(CardHposition, 0);//卡片物件添加位置
+        #endregion
+    }
+    #endregion
+
+
+
+}
---
+++ 卡牌練習/Assets/CardManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    #region 宣告卡牌玩家 與卡牌主機
+    public Card CardPlayer ,CardPC;
+    #endregion
+    #region 宣告卡牌花紋
+    //public Sprite Image9, Image3;
+    //public Sprite Image0, Image1, Image2, Image4, Image5, Image6, Image7, Image8;
+    public Sprite[] images;
+    #endregion
+    #region 宣告文字提示文字
+    public TextMesh tip;
+    public TextMesh start;
+    #endregion 
+    
+    void Start()  //unity C#主程式入口
+    {
+        int rplayer = Random.Range(0, 10);
+        int rpc = Random.Range(0, 10);
+        // Debug.Log("玩家隨機值:" + rplayer);
+        #region 初始玩家在左位置-5 電腦在右位置5 玩家牌面9  電腦牌面3,牌面花紋套用圖片
+        CardPlayer = new Card(rplayer, -5, images[rplayer],"玩家卡片");
+        CardPC = new Card(rpc, 5, images[rpc],"電腦卡片");
+        #endregion
+        # region 顯示提示比較勝負文字內容
+        if (CardPlayer.CardNum > CardPC.CardNum)
+        {
+           tip.text = "玩家勝";
+        }
+        else if( CardPlayer.CardNum < CardPC.CardNum)
+        {
+            tip.text = "電腦勝";
+        }
+        else{
+            tip.text = "平手";
+        }
+
+        tip.fontSize = 100;
+        tip.color = Color.red;
+        #endregion
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+
+            Start();
+        }
+    }
+
+}
+
